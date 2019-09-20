4c2b063986e498016580613ec99bc92e7909a098
Alexander Collings [Student] <alexander.collings@student.sl.on.ca>
2019-09-20 13:48:44 -0400

    Made Marine's head bobble

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody head;
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
 
@@ -20,5 +21,20 @@ public class PlayerController : MonoBehaviour
                                             0,
                                             Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+                                            0,
+                                            Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            // TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
